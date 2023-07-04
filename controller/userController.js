ceef29b088994a84598f97e4b716fecdce42af0a
princedraculla <amirtorkashvand56@gmail.com>
2023-07-04 23:36:31 +0330

    update user API added

--- controller/userController.js
+++ controller/userController.js
@@ -1,63 +1,82 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
- const addUser = async (req,res) => {
-    const { name, email } = req.body;
-    try {
-        const addedUser = await prisma.user.create({
-            data: {
-                name: name,
-                email: email
-            }
-        })
-        res.status(201).json(addedUser)
-    } catch (error) {
-        res.status(400).json({ msg: error.message })
-    }
-}
+const addUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const addedUser = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+      },
+    });
+    res.status(201).json(addedUser);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
 
-const addAdmin = async (req,res) => {
-    const { name, email, role } = req.body;
-    
-    try {
-        const addedAdmin = await prisma.user.create({
-          data: {
-            name: name,
-            email: email,
-            role: role
-    }})
-        res.status(200).json(addedAdmin)
-    } catch (error) {
-        res.status(400).json({msg: error.message})        
-    }
-}
+const updateUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: name,
+        email: email,
+      },
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
 
-const findAllUser = async (req,res) => {
-    try {
-            const allUsers = await prisma.user.findMany();
-                res.status(200).json(allUsers)
-    } catch (error) {
-        res.status(400).json({ msg: error.message})
-    }
-}
+const addAdmin = async (req, res) => {
+  const { name, email, role } = req.body;
 
-const findUserById = async (req,res) => {
-        try {
-        const uniqueUser = await prisma.user.findUnique({
-            where: 
-            {
-                id: Number(req.params.id)
-            }
-        })
-        res.status(200).json(uniqueUser)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const addedAdmin = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        role: role,
+      },
+    });
+    res.status(200).json(addedAdmin);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+const findAllUser = async (req, res) => {
+  try {
+    const allUsers = await prisma.user.findMany();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+const findUserById = async (req, res) => {
+  try {
+    const uniqueUser = await prisma.user.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(uniqueUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    addUser,
-    findAllUser,
-    findUserById,
-    addAdmin
-}
+  addUser,
+  updateUser,
+  findAllUser,
+  findUserById,
+  addAdmin,
+};
--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addUser, findAllUser, findUserById, addAdmin } = require("./controller/userController")
+const { addUser, updateUser, findAllUser, findUserById, addAdmin } = require("./controller/userController")
 
-router.post('/add-admin', addAdmin)
+router.post('/add-admin', addAdmin);
 router.post('/add', addUser);
-router.get('/user-list', findAllUser)
-router.get('/user/:id', findUserById)
+router.patch('/update/:id', updateUser);
+router.get('/user-list', findAllUser);
+router.get('/user/:id', findUserById);
 
 module.exports = router;
